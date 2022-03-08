c5aba95e3075f7e45178135d13b1d6f5e6bdc1e0
MoulaDilshan <skmoula12@gmail.com>
2022-03-08 17:52:37 +0530

    php

---
+++ datatypes.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    </head>
+    <body>
+        <h1> Php Data Types:</h1>
+        <?php
+            $cars = array("volvo", "Volksvagan", "swift");
+            var_dump($cars);
+        ?>
+    </body>
+    
+</html>
